41aa0e6896748b514b7a3526d03a6b8cf646d1ff
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-10-13 23:49:50 +0300

    add mobile menu

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+
+const mobileMenu = document.querySelector('.js-mobile-menu');
+const openMenuBtn = document.querySelector('.js-open-menu');
+const closeMenuBtn = document.querySelector('.js-close-menu');
+
+const toggleMenu = (ev) => {
+   
+  const isMenuOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+  mobileMenu.classList.toggle('is-open');
+  
+};
+
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
+mobileMenu.addEventListener('ckick', toggleMenu);
