2a0f737af8488100e9dc752faf87aec8585924e5
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-10-13 20:50:29 +0000

    Deploying to gh-pages from @ mariastetciuk/nails-project@41aa0e6896748b514b7a3526d03a6b8cf646d1ff 🚀

---
+++ index.179538bf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-mobile-menu"),t=document.querySelector(".js-open-menu"),n=document.querySelector(".js-close-menu"),c=n=>{const c="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!c),e.classList.toggle("is-open")};t.addEventListener("click",c),n.addEventListener("click",c),e.addEventListener("ckick",c);
+//# sourceMappingURL=index.179538bf.js.map
---
+++ index.a38d7bf8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-mobile-menu"),t=document.querySelector(".js-open-menu"),n=document.querySelector(".js-close-menu"),c=function(n){var c="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!c),e.classList.toggle("is-open")};t.addEventListener("click",c),n.addEventListener("click",c),e.addEventListener("ckick",c)}();
+//# sourceMappingURL=index.a38d7bf8.js.map
